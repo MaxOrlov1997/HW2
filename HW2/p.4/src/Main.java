8a1d2a47f094725555a5d74d55947acd780858e8
MaxOrlov1997 <maxorlov337@gmail.com>
2023-03-19 18:25:59 +0200

    agg HW2

--- HW2/p.4/src/Main.java
+++ HW2/p.4/src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
-        if (Math.abs(num1)>Math.abs(num2) && Math.abs(num2)>Math.abs(num3)) {
-            System.out.print(num1);}
-        else {
-            System.out.println((Math.abs(num2)> Math.abs(num3))? num2:num3);
+        boolean res = Math.abs(num1)>Math.abs(num2) ? true : false;
+        boolean res1 = Math.abs(num1)>Math.abs(num3) ? true : false;
+        if (res==res1) {
+            System.out.println(num1);
         }
-
+         else { System.out.println((Math.abs(num2)> Math.abs(num3))? num2:num3);}
 
         print(num2);
 
