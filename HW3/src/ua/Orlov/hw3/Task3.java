53bc4860054c5df400b3569d3a4742bd5f32127d
MaxOrlov1997 <maxorlov337@gmail.com>
2023-03-24 15:31:13 +0200

    add HW3

--- HW3/src/ua/Orlov/hw3/Task3.java
+++ HW3/src/ua/Orlov/hw3/Task3.java
@@ -11,7 +11,7 @@ public class Task3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String line = scan.nextLine();
-        int amount = 0;
+        int amount=0;
         System.out.println(word(line, amount));
     }
     public static int word(String line, int amount) {
@@ -19,7 +19,8 @@ public class Task3 {
         for (int i = 1; line.length() > i; i++) {
             if (line.charAt(i) == ' ' && line.charAt(i - 1) != ' ') {
                 ++amount;
-            } else {
+            }
+            else {
                 continue;
             }
         }
