303a288cf9d6c0b7c39ade0f2c6479fd6f36abba
MaxOrlov1997 <maxorlov337@gmail.com>
2023-03-25 17:20:44 +0200

    add HW3

--- HW3/src/ua/Orlov/hw3/Task3.java
+++ HW3/src/ua/Orlov/hw3/Task3.java
@@ -1,6 +1,6 @@
 package ua.Orlov.hw3;
 
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 /*3. Подсчитать количество слов в строке
@@ -11,17 +11,18 @@ public class Task3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String line = scan.nextLine();
-        int amount=0;
+        int amount = 0;
         System.out.println(word(line, amount));
     }
+
     public static int word(String line, int amount) {
 
         for (int i = 1; line.length() > i; i++) {
             if (line.charAt(i) == ' ' && line.charAt(i - 1) != ' ') {
                 ++amount;
-            }
-            else {
-                continue;
+            } else if (line.isBlank()) {
+                return amount;
+
             }
         }
         return ++amount;
