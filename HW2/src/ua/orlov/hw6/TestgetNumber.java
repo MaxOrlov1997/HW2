a69ef5515b1b2fda9435ec760a04a437c0f76ecd
MaxOrlov1997 <maxorlov337@gmail.com>
2023-04-05 21:22:49 +0300

    fix folgers

--- HW2/src/ua/orlov/hw6/TestgetNumber.java
+++
@@ -1,9 +0,0 @@
-package ua.orlov.hw6;
-
-public class TestgetNumber {
-    public static void main(String... args){
-        Phone phone= new Phone();
-        phone.getNumber= "55445544";
-        System.out.println(phone.getNumber);
-    }
-}
