0c90d5bdf73aa5164aa0e38e7901b95423c8bf97
MaxOrlov1997 <maxorlov337@gmail.com>
2023-04-08 17:12:12 +0300

    add hw2

---
+++ src/ua/orlov/hw2/Task3.java
@@ -0,0 +1,20 @@
+package ua.orlov.hw2;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        int num = 10;
+        evenNum(num);
+        System.out.println(num);
+    }
+
+    public static int evenNum(int even) {
+        if (even % 2 == 0) {
+            System.out.println("Четное");
+            return even;
+        } else
+            System.out.println("Не четное");
+        return even;
+    }
+}
+
