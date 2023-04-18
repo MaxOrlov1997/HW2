816ba6e2648711e0832622c8a38cfd388b913be9
MaxOrlov1997 <maxorlov337@gmail.com>
2023-04-18 19:37:13 +0300

    test hw10

---
+++ src/test/java/ua/orlov/hw10/testStudentAspirant.java
@@ -0,0 +1,33 @@
+package ua.orlov.hw10;
+
+import org.junit.Assert;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class testStudentAspirant {
+
+
+    @Test
+    public void testStudent(){
+        Student student = new Student();
+        Assertions.assertEquals(100, student.getScholarship(student.avaregeMark) );
+    }
+    @Test
+    public void testStudentinvalid(){
+        Student student = new Student();
+        Assertions.assertEquals(70, student.getScholarship(student.avaregeMark) );
+    }
+    @Test
+    public void testStudentMynum(){
+        Student student = new Student();
+        double num= 4.5;
+        Assertions.assertEquals(70, student.getScholarship(num) );
+    }
+    @Test
+    public void testAspirantMynum(){
+        Student student = new Aspirant();
+        double num= 4.5;
+        Assertions.assertEquals(180, student.getScholarship(num) );
+    }
+}
