62ca89628093a009761046b52633f5df47e0fab2
MaxOrlov1997 <128231077+MaxOrlov1997@users.noreply.github.com>
2023-04-19 17:24:46 +0300

    Add files via upload

--- src/ua/orlov/hw5/Task2.java
+++ src/ua/orlov/hw5/Task2.java
@@ -1,6 +1,7 @@
 package ua.orlov.hw5;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Task2 {
@@ -12,17 +13,24 @@ public class Task2 {
         System.out.println(sortArray(array));
     }
 
-    public static int[] fillArray(int[] arrays) {
-        for (int j = 0; j < arrays.length; j++) {
-            arrays[j] = ThreadLocalRandom.current().nextInt(0, 10);
+    public static int[] fillArray(int[] array) {
+        for (int j = 0; j < array.length; j++) {
+            array[j] = ThreadLocalRandom.current().nextInt(0, 10);
         }
-        return arrays;
+        return array;
     }
 
     public static boolean sortArray(int[] array) {
         int[] sorted = Arrays.copyOf(array, array.length);
+        int[] sor = new int [array.length];
+        int n=0;
         Arrays.sort(sorted);
-        boolean sort = array.equals(sorted);
+        for (int i = sorted.length - 1; i >= 0; i--) {
+           sor[n] = sorted[i];
+            n++;
+        }
+        System.out.println(Arrays.toString(sor));
+        boolean sort = Arrays.toString(array).equals(Arrays.toString(sor));
         return sort;
     }
 }
