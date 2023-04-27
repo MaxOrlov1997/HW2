87e9bdd7bffe0880b86d06ce3ba55fcbfb1097e2
MaxOrlov1997 <maxorlov337@gmail.com>
2023-04-27 19:31:44 +0300

    hw12

--- src/main/java/ua/orlov/hw12/Task3.java
+++ src/main/java/ua/orlov/hw12/Task3.java
@@ -1,18 +1,29 @@
 package ua.orlov.hw12;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Task3 {
-    String test = "Ha";
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            firstExtend();
+        } catch (Exception a) {
+            System.out.println(a);
+        }
+    }
+
+    private static void firstExtend() throws Exception {
+        int test = ThreadLocalRandom.current().nextInt(0, 4);
+        if (test == 0) {
+            throw new Exception("Тут ноль");
+        }
+        if (test == 3) {
+            throw new Exception("Тут три");
+        }
+        if (test == 2) {
+            throw new Exception("Тут два");
+        }
 
     }
-private static void firstExtend(){
-
 }
-    private static void secondExtend(){
 
-    }
-    private static void lastExtend(){
-
-    }
-
-}
