83cb892b55a987757eb83572d7c1b548001424b7
MaxOrlov1997 <maxorlov337@gmail.com>
2023-04-30 12:54:22 +0300

    hw13

--- src/main/java/ua/orlov/hw13/Runner.java
+++ src/main/java/ua/orlov/hw13/Runner.java
@@ -5,24 +5,23 @@ import java.util.Arrays;
 public class Runner {
     public static void main(String[] args) {
         SortBubble sortBubble = new SortBubble();
-        int[] array = sortBubble.array;
+        int[] array = {5,3,4,2,1,6};
         long start = System.nanoTime();
         int[] resBubble = sortBubble.sortBubble(array);
-        // специально пишу так, для того если нужно изменить массив то только в одном месте
         long finish = System.nanoTime();
         long res = finish - start;
         System.out.println(Arrays.toString(resBubble));
         System.out.println("Sort bubble - " + res);
 
         long startQuick = System.nanoTime();
-        int[] resQuickBubble = sortBubble.quickSort(array, 1, array.length - 1);
+        int[] resQuickBubble = sortBubble.quickSort(array, 0, array.length - 1);
         long finishQuick = System.nanoTime();
         long resQuick = finishQuick - startQuick;
         System.out.println("Quick sort - " + resQuick);
         System.out.println(Arrays.toString(resQuickBubble));
 
         BinarySearch ob = new BinarySearch();
-        int arr[] = sortBubble.array;
+        int arr[] = {1,2,3,4,5,6};
         int x = 5;
         long startBinary = System.nanoTime();
         int result = ob.binarySearch(arr, x);
