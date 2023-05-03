27c474099cf85cbbb3b277f72cb445bcde404722
MaxOrlov1997 <maxorlov337@gmail.com>
2023-05-03 17:15:15 +0300

    hw11

--- src/main/java/ua/orlov/hw11/Runner.java
+++ src/main/java/ua/orlov/hw11/Runner.java
@@ -3,14 +3,14 @@ package ua.orlov.hw11;
 public class Runner {
     public static void main(String[] args) {
         Car car = new Car();
-        car.toMove();
+        car.move();
         System.out.println(car.toString());
         car.statistics();
         PassengerCar passengerCar = new PassengerCar();
-        passengerCar.putPassenger();
+        passengerCar.putPassenger(3);
         Truck truck = new Truck();
-        truck.submergeCargo();
-        truck.toMove();
-        truck.putPassengerTruck();
+        truck.move();
+        truck.submergeCargo(200, 500);
+        truck.unloadingCar(300);
     }
 }
