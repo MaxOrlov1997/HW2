97c49fd6f9c9c1d21eeffc37c4d1f57675280d99
MaxOrlov1997 <maxorlov337@gmail.com>
2023-05-07 15:59:19 +0300

    hw15

--- src/main/java/ua/orlov/hw15/Box.java
+++ src/main/java/ua/orlov/hw15/Box.java
@@ -3,23 +3,49 @@ package ua.orlov.hw15;
 import java.util.*;
 
 public class Box {
-    private static List<Integer> volume = new ArrayList<>();
+    private Integer volume;
+
+    public Box(Integer volume) {
+        this.volume = volume;
+    }
+
+    int getAge() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "volume=" + volume +
+                '}';
+    }
 
     public static void main(String[] args) {
-        volume.add(12);
-        volume.add(18);
-        volume.add(15);
-        volume.add(17);
-        volume.add(0);
-        TreeSet<Integer> tr = new TreeSet<>();
-        ListIterator<Integer> it = volume.listIterator();
-        while (it.hasNext()){
-            tr.add(it.next());
-        }
-        if(tr.first()==0){
-        Integer first = tr.pollFirst();}
-        TreeSet<Integer> t = (TreeSet<Integer>) tr.descendingSet();
+        Comparator<Box> comparator = new Boxvolume();
+        TreeSet<Box> tr = new TreeSet<>(comparator);
+        tr.add(new Box(12));
+        tr.add(new Box(15));
+        tr.add(new Box(18));
+        tr.add(new Box(11));
+        tr.add(new Box(0));
+        System.out.println(tr);
 
-        System.out.println(t);
     }
 }
+
+class Boxvolume implements Comparator<Box> {
+    public int compare(Box a, Box b) {
+
+        if (a.getAge() == 0)
+            return -1;
+        if (b.getAge() == 0)
+            return -1;
+        if (a.getAge() > b.getAge())
+            return -1;
+        if (a.getAge() < b.getAge())
+            return 1;
+        else
+            return 0;
+    }
+}
+
