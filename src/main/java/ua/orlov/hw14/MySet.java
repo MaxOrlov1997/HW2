de9eaeb022399e20f5dd37fb784b45a85a2471f4
MaxOrlov1997 <maxorlov337@gmail.com>
2023-05-11 14:27:56 +0300

    hw14

--- src/main/java/ua/orlov/hw14/MySet.java
+++ src/main/java/ua/orlov/hw14/MySet.java
@@ -3,44 +3,45 @@ package ua.orlov.hw14;
 import java.util.Arrays;
 
 public class MySet<T> {
-    Integer size;
-    private T[] generic = (T[]) new Integer[]{3, 4, 5};
+    private T[] generic;
 
-    public MySet(T[] generic) {
-        this.generic = generic;
+    public MySet(int size) {
+        this.generic = (T[]) new Object[size];
     }
 
-    public MySet(Integer size) {
-        this.size = size;
-    }
-
-    public MySet() {
-
-    }
 
     public void add(T number) {
         for (int i = 0; i < generic.length; i++) {
-            if (generic[i]==number){
+            if (generic[i] == number) {
+                System.out.println("Есть такой елемент " + number);
                 return;
-            }}
-        if (size > generic.length-1) {
-            generic[generic.length] = number;
+            }
+            if (generic[i] == null) {
+                generic[i] = number;
+                return;
+            }
         }
     }
 
     public T get(int index) {
         return generic[index];
     }
-    public String toString() {
-        return Arrays.toString(generic);
-    }
 
     public static void main(String[] args) {
-        MySet<Integer> mySet = new MySet();
-        mySet.size=4;
+        MySet<Object> mySet = new MySet(3);
+        mySet.add(5);
+        mySet.add(3);
+        mySet.add(2);
         mySet.add(2);
         System.out.println(mySet.generic.length);
         System.out.println(mySet.get(0));
         System.out.println(mySet.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "MySet{" +
+                "generic=" + Arrays.toString(generic) +
+                '}';
     }
 }
