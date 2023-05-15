c00a3907c4955d2d43c79d935977510c1f53ff25
MaxOrlov1997 <maxorlov337@gmail.com>
2023-05-15 21:32:35 +0300

    hw12

--- src/main/java/ua/orlov/hw12/Exceptions.java
+++ src/main/java/ua/orlov/hw12/Exceptions.java
@@ -1,14 +1,17 @@
 package ua.orlov.hw12;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.SocketException;
 import java.util.concurrent.ThreadLocalRandom;
 
-class Exception {
+class Exception extends Throwable {
     static int test = ThreadLocalRandom.current().nextInt(1, 4);
 
     public static void main(String[] args) {
         try {
             throwRandomException();
-        } catch (IllegalAccessError a) {
+        } catch (RuntimeException a) {
             System.out.println(a);
         }
     }
@@ -18,10 +21,10 @@ class Exception {
             throw new NumberFormatException("Тут один");
         }
         if (test == 2) {
-            throw new NumberFormatException("Тут два");
+            throw new IllegalArgumentException("Тут два");
         }
         if (test == 3) {
-            throw new NumberFormatException("Тут три");
+            throw new IndexOutOfBoundsException("Тут три");
         }
     }
 }
