bb3d54b11c68b7dce4114c209e97b13442ac0ac2
MaxOrlov1997 <maxorlov337@gmail.com>
2023-05-18 13:19:10 +0300

    hw14task2

--- src/main/java/ua/orlov/hw14/Task2MySet.java
+++ src/main/java/ua/orlov/hw14/Task2MySet.java
@@ -1,39 +1,26 @@
 package ua.orlov.hw14;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Task2MySet<T> {
-    private static List list;
 
-    public Task2MySet(List<T> list) {
-        this.list = list;
+    public Task2MySet() {
+
     }
 
     public static void main(String[] args) {
-        Task2MySet mySetStat = new Task2MySet(new ArrayList<>());
-        mySetStat.add(12);
-        mySetStat.add("Name");
-        System.out.println(mySetStat.list);
-        System.out.println(Arrays.asList(mySetStat.addMas(new Object[]{"5", "Max", 7})));
-        System.out.println(Arrays.asList(list));
-
+        add(12);
+        add("Name");
+        Task2MySet task2MySet = new Task2MySet();
+        task2MySet.addMas(new Object[]{2, "Name", 4.5});
     }
 
     private static <T> void add(T nam) {
-        list.add(nam);
+        System.out.println(nam);
     }
 
     private T[] addMas(T[] array) {
-        list.add((T) array);
-        return array;
-    }
-
-    @Override
-    public String toString() {
-        return "MySetNoStat{" +
-                "list=" + list +
-                '}';
+        System.out.println(Arrays.toString(array));
+        return null;
     }
 }
