90894ab911e295816c14b89a41bff9b0c9a414da
MaxOrlov1997 <maxorlov337@gmail.com>
2023-05-18 22:48:11 +0300

    hw17 Box

--- src/main/java/ua/orlov/hw17/Box.java
+++ src/main/java/ua/orlov/hw17/Box.java
@@ -1,31 +1,43 @@
 package ua.orlov.hw17;
-
+/*Спроектировать класс Box который содержит коллекцию Item, size.
+Item должен содержать поле name и cost
+a. Создать коллекцию Box и инициализировать ее 10 коробками
+b. Спроектировать метод, который поможет определить подходит ли
+коробка по размеру.
+Boolean isSuitable(minSizeLimit)
+c. Применить фильтр к коллекции коробок по размеру,
+d. У оставшихся коробок взять коллекции Item
+e. Отсортировать по цене
+f. Вывести цены в консоль*/
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class Box {
-    static List list = new ArrayList<>();
-    int size = 2;
-    static Collection[] box = new ArrayList[]{new ArrayList<>()};
-
-    public Box(int size) {
-        this.box = new Collection[]{box[size]};
-    }
+   static int size = 10;
+     static ArrayList box = new ArrayList();
 
     public static void main(String[] args) {
         Item item = new Item();
-        list.add(item);
-//        box.(item);
-//        box.(item);
-//        box.(item);
-//        box.(item);
-        
+        for (int i = 0; i < size; i++) {
+            box.add(item);
+        }
+//    box.stream().
+//            filter(it -> true==isSuitable()).
+//            collect(Collectors.toCollection(Item ))
+
+
+
+    }
+    private static Boolean isSuitable(){
+       if(box.size()<=size)
+       {return !box.isEmpty();}
+       else return false;
     }
 }
 class Item{
     String name = "Apple";
-    Double cost = 4.5;
+    Double cost = ThreadLocalRandom.current().nextDouble(1,10);
 
     @Override
     public String toString() {
