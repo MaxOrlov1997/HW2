516b317e33e499093f32889b09100002d4e56341
MaxOrlov1997 <maxorlov337@gmail.com>
2023-06-08 12:40:51 +0300

    hw17

--- src/main/java/ua/orlov/hw17/Task2.java
+++ src/main/java/ua/orlov/hw17/Task2.java
@@ -10,8 +10,8 @@ public class Task2 {
     public static void main(String[] args) {
         System.out.println(fillArray().stream()
                 .filter(it -> it >= 0)
-                .mapToInt(Integer::intValue).
-                summaryStatistics());
+                .mapToInt(Integer::intValue)
+                .summaryStatistics());
     }
 
     public static List<Integer> fillArray() {
